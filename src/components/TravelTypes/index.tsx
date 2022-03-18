70b120ef6c19fb0883b701f176784b039b4f517f
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-03-18 18:22:14 -0300

    fixed Bugs :bug:

--- src/components/TravelTypes/index.tsx
+++ src/components/TravelTypes/index.tsx
@@ -7,7 +7,7 @@ export default function TravelTypes(){
       templateColumns={["1fr 1fr", "1fr 1fr", "1fr 1fr", "repeat(5, 1fr)"]}
       w="100%"
       justifyContent="space-between"
-      align="center"
+      //align="center"
       mt={["10","32"]}
       mx="auto"
       maxW="1160px"
